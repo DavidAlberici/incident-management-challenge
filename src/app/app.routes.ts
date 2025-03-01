4a242ef37f939a009d37d8de1d1fe96b667f1560
David Alberici <davidalberici3200@gmail.com>
2025-03-01 13:12:31 +0100

    Added auth pages UI scaffold

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SigninComponent } from './authentication/signin/signin.component';
+import { SignupComponent } from './authentication/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [  
+   { path: '', component: SigninComponent },
+   { path: 'signin', component: SigninComponent },
+   { path: 'signup', component: SignupComponent }
+];
