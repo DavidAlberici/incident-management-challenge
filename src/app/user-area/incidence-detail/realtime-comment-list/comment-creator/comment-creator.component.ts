6752493ff573feb90f93fcae5af2652e5e2457e8
David Alberici <davidalberici3200@gmail.com>
2025-03-03 09:42:34 +0100

    Implemented comment creator logic (connected it to firestore)

--- src/app/user-area/incidence-detail/realtime-comment-list/comment-creator/comment-creator.component.ts
+++ src/app/user-area/incidence-detail/realtime-comment-list/comment-creator/comment-creator.component.ts
@@ -1,16 +1,24 @@
-import { Component, inject } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, inject, Input } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { addDoc, collection } from '@firebase/firestore';
+import { NotificationDialogService } from '../../../../notification-dialog/services/notification-dialog.service';
 
 @Component({
   selector: 'app-comment-creator',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './comment-creator.component.html',
   styleUrl: './comment-creator.component.css'
 })
 export class CommentCreatorComponent {
+  @Input() incidenceId!: string;
   public commentForm: FormGroup;
   public errorMessage: string = "";
+  public isLoading: boolean = false;
   private fb: FormBuilder = inject(FormBuilder)
+  private firestore: Firestore = inject(Firestore)
+  private notificationDialogService: NotificationDialogService = inject(NotificationDialogService)
 
   constructor() {
     this.commentForm = this.fb.group({
@@ -18,7 +26,27 @@ export class CommentCreatorComponent {
     });
   }
 
-  createComment() {
-    //TODO
+  async submitComment(): Promise<void> {
+    this.isLoading = true;
+    if (this.commentForm.invalid) {
+      this.isLoading = false;
+      this.errorMessage = "Comment content must be at least 10 characters long"
+      return;
+    }
+    let created: boolean = await this.createComment();
+    if (created) {
+      this.notificationDialogService.notifySuccess("The comment was created successfully!")
+      this.commentForm.reset()
+    }
+    this.isLoading = false;
+  }
+
+  private async createComment(): Promise<boolean> {
+    const commentsCollection = collection(this.firestore, `incidence/${this.incidenceId}/comment`);
+    await addDoc(commentsCollection, {
+      content: this.commentForm.value.content,
+      createdDate: new Date().toISOString(),
+    });
+    return true;
   }
 }
