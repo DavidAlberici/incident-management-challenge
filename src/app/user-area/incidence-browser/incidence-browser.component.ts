f245bd61fbf68baeafa08e7a9db5596b4132f190
David Alberici <davidalberici3200@gmail.com>
2025-03-03 06:02:07 +0100

    Added firebase to incidence browser. First iteration completed

--- src/app/user-area/incidence-browser/incidence-browser.component.ts
+++ src/app/user-area/incidence-browser/incidence-browser.component.ts
@@ -1,45 +1,58 @@
-import { NgForOf } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgForOf, NgIf } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { Incidence, priorityList, statusList } from '../../model/incidence';
+import { collection, Firestore, getDocs, or, query, QueryCompositeFilterConstraint, QueryFilterConstraint, where } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-incidence-browser',
-  imports: [NgForOf],
+  imports: [NgForOf, NgIf, FormsModule],
   templateUrl: './incidence-browser.component.html',
   styleUrl: './incidence-browser.component.css'
 })
-export class IncidenceBrowserComponent {
+export class IncidenceBrowserComponent implements OnInit {
   public statusList: string[] = statusList
   public priorityList: string[] = priorityList
+  public selectedStatus: string = "Any";
+  public selectedPriority: string = "Any";
+  public incidenceList: Incidence[] | undefined;
+  public isLoading: boolean = true;
+  private firestore: Firestore = inject(Firestore)
 
+  ngOnInit(): void {
+    this.getIncidenceList()
+  }
 
-  public incidenceList: Incidence[] = [
-    new Incidence(
-      '1', // id
-      'Incidence 1', // title
-      'incidence-1', // url
-      'This is a sample description for Incidence 1. It can be quite long and should wrap text properly.', // description
-      'Open', // status
-      'High', // priority
-      new Date().toISOString()
-    ),
-    new Incidence(
-      '2', // id
-      'Incidence 2', // title
-      'incidence-2', // url
-      'This is another sample description for Incidence 2. It should also wrap text properly.', // description
-      'Closed', // status
-      'Medium', // priority
-      new Date().toISOString()
-    ),
-    new Incidence(
-      '3', // id
-      'Incidence 3', // title
-      'incidence-3', // url
-      'Yet another sample description for Incidence 3. Text wrapping is important here too.', // description
-      'In Progress', // status
-      'Low', // priority
-      new Date().toISOString()
-    ),
-  ];
+  public applyFilters(): void {
+    this.getIncidenceList()
+    console.log("this.selectedStatus " + this.selectedStatus)
+    console.log("this.selectedPriority " + this.selectedPriority)
+  }
+
+  private getIncidenceList(): void {
+    this.isLoading = true;
+    const incidenceCollection = collection(this.firestore, 'incidence');
+    const q = query(incidenceCollection, this.getCompositeFilter());
+    getDocs(q).then(docs => {
+      this.isLoading = false;
+      this.incidenceList = docs.docs.map(doc => {
+        return {
+          ...doc.data(),
+          id: doc.id,
+        } as Incidence
+      })
+    });
+  }
+
+  private getCompositeFilter(): QueryCompositeFilterConstraint {
+    const filterList: QueryFilterConstraint[] = []
+    if (this.selectedStatus != "Any" && this.selectedStatus != undefined) {
+      filterList.push(where("status", "==", this.selectedStatus))
+    }
+    if (this.selectedPriority != "Any" && this.selectedPriority != undefined) {
+      filterList.push(where("priority", "==", this.selectedPriority))
+    }
+    console.log(filterList)
+    return or(...filterList)
+  }
 }
